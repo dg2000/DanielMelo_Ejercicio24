45005ddd5731985c061fe8c9a5b1a20a1f78beb4
daniel <dg.melo@uniandes.edu.co>
2018-05-03 07:55:32 -0500

    h

---
+++ proba.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+def proba(x, landa):
+
+    norma = 1/(np.exp(-1) - np.exp(-20))
+    return (norma/landa)*np.exp(-x/landa)
+
+graf = np.linspace(1e-6, 100.0, 2000)
+
+pos = [1.2, 2.5, 2.8, 5.0]
+
+p = np.ones(len(graf))
+
+
+for i in range(len(graf)):
+    for j in range(len(pos)):
+
+        p[i] = p[i] * proba(pos[j], graf[i])
+
+
+plt.plot(graf, p)
+plt.show()
+
+
+
+
